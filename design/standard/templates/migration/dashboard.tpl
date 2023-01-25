bc42d78a6f7621a90eb4efea86c6b66492961c61
Luca Realdi <luca.realdi@opencontent.it>
2023-01-25 16:28:03 +0100

    Bugfix in migration template call

--- design/standard/templates/migration/dashboard.tpl
+++ design/standard/templates/migration/dashboard.tpl
@@ -1,9 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html>
+<!doctype html>
+<html lang="it">
 <head>
     <title>Assistente migrazione</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     {ezscript_load(array(
         'ezjsc::jquery',
         'ezjsc::jqueryUI',
--- modules/migration/dashboard.php
+++ modules/migration/dashboard.php
@@ -94,7 +94,6 @@ if ($http->hasGetVariable('fields')) {
     eZExecution::cleanExit();
 }
 
-
 if ($http->hasGetVariable('status')) {
     header('Content-Type: application/json');
     header('HTTP/1.1 200 OK');
@@ -122,8 +121,21 @@ if ($http->hasGetVariable('action')) {
     eZExecution::cleanExit();
 }
 
-
-echo $tpl->fetch('design:migration/dashboard.tpl');
-
-//eZDisplayDebug();
-eZExecution::cleanExit();
+$Result = [];
+$Result['content'] = $tpl->fetch('design:migration/dashboard.tpl');
+$Result['path'] = [];
+$contentInfoArray = [
+    'node_id' => null,
+    'class_identifier' => null,
+];
+$contentInfoArray['persistent_variable'] = [
+    'show_path' => false,
+];
+if (is_array($tpl->variable('persistent_variable'))) {
+    $contentInfoArray['persistent_variable'] = array_merge(
+        $contentInfoArray['persistent_variable'],
+        $tpl->variable('persistent_variable')
+    );
+}
+$Result['content_info'] = $contentInfoArray;
+$Result['pagelayout'] = false;
