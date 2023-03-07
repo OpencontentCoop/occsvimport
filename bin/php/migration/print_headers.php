a41fce190b645ba20626a273750e123748b70eb5
Luca Realdi <luca.realdi@opencontent.it>
2023-03-07 14:37:06 +0100

    Improve ux in check header script

--- bin/php/migration/print_headers.php
+++ bin/php/migration/print_headers.php
@@ -23,25 +23,40 @@ $options = $script->getOptions(
 $script->initialize();
 $script->setUseDebugAccumulators(true);
 
-$client = new GoogleSheetClient();
+$client = OCMigrationSpreadsheet::instanceGoogleSheetClient();
 $googleSheetService = $client->getGoogleSheetService();
 $spreadsheetId = OCMigrationSpreadsheet::getConnectedSpreadSheet();
 $spreadsheet = new GoogleSheet($spreadsheetId);
 
 foreach (OCMigration::getAvailableClasses($options['only'] ? explode(',', $options['only']) : []) as $className) {
-    $cli->output($className);
     try {
         $row = $spreadsheet->getSheetDataHash($className::getSpreadsheetTitle())[0];
+        unset($row['IGNORA']);
+        $sheetHeaders = array_keys($row);
+
         /** @var ocm_interface $test */
         $test = new $className;
-        print_r(
-            array_diff(
-                array_keys($row),
-                array_keys($test->toSpreadsheet())
-            )
+        $mappedHeaders = array_keys($test->toSpreadsheet());
+        $diff = array_diff(
+            $sheetHeaders,
+            $mappedHeaders
         );
+        if (!empty($diff)){
+            $cli->warning($className);
+            foreach ($diff as $head){
+                $cli->warning(' - ' . $head);
+            }
+        }else{
+            $cli->output($className . ' ok');
+        }
+
+        if ($options['verbose']){
+            var_dump($sheetHeaders);
+            var_dump($mappedHeaders);
+        }
+
     }catch (Exception $e){
-        $cli->error($e->getMessage());
+        $cli->error($className . ' ' . $e->getMessage());
     }
 }
 
