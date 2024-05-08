d3a46dc26585a671410237c5fb3c8c31c6f52e76
Luca Realdi <luca.realdi@opencontent.it>
2024-05-08 21:35:23 +0200

    Add set spreadsheet

---
+++ bin/php/migration/set_spreadsheet.php
@@ -0,0 +1,58 @@
+<?php
+
+require 'autoload.php';
+
+$cli = eZCLI::instance();
+$script = eZScript::instance([
+    'description' => (""),
+    'use-session' => false,
+    'use-modules' => true,
+    'use-extensions' => true,
+]);
+
+$script->startup();
+$options = $script->getOptions(
+    "[master:][name:][force]",
+    "",
+    [
+        'master' => 'Master url',
+        'name' => 'Name prefix',
+        'force' => 'Force reconnect',
+    ]
+);
+$script->initialize();
+$script->setUseDebugAccumulators(true);
+
+$connected = OCMigrationSpreadsheet::getConnectedSpreadSheet();
+if ($connected && !$options['force']){
+    $cli->output("Spreadsheet already connected " . $connected);
+    $script->shutdown(0);
+}
+
+$siteName = eZINI::instance()->variable('SiteSettings', 'SiteName');
+$siteName = str_ireplace('Comune di ', '', $siteName);
+$name = $options['name'] ?? '';
+$name .= ' - ' . $siteName;
+
+$client = new OCMGoogleSheetClient();
+$user = $client->getCredentials()['client_email'] ?? '?';
+$cli->warning('User ' . $user);
+
+$spreadsheetUrl = $options['master'] ?? die('Missing master');
+$masterSpreadsheetId = OCGoogleSpreadsheetHandler::getSpreadsheetIdFromUri($spreadsheetUrl);
+$masterSpreadsheet = new \Opencontent\Google\GoogleSheet($masterSpreadsheetId, $client);
+
+$googleClient = $client->getGoogleClient();
+$googleClient->setScopes([Google_Service_Sheets::SPREADSHEETS, Google_Service_Drive::DRIVE]);
+$serviceDrive = new Google_Service_Drive($googleClient);
+$file = new Google_Service_Drive_DriveFile();
+$file->setName($name);
+try {
+    $spreadsheet = $serviceDrive->files->copy($masterSpreadsheetId, $file);
+    $cli->warning('Sheet: ' . $spreadsheet->getId() . ' ' . $spreadsheet->getName());
+    OCMigrationSpreadsheet::setConnectedSpreadSheet($spreadsheet->getId());
+} catch (Throwable $e) {
+    $cli->error($e->getMessage());
+}
+
+$script->shutdown();
