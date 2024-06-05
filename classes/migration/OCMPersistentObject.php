f571efe17106de6b5e2247d2517b5e3e62694f34
Luca Realdi <luca.realdi@opencontent.it>
2024-06-05 12:35:34 +0200

    Disable ssl verify in import

--- classes/migration/OCMPersistentObject.php
+++ classes/migration/OCMPersistentObject.php
@@ -626,6 +626,7 @@ abstract class OCMPersistentObject extends eZPersistentObject implements ocm_int
         curl_setopt($ch, CURLOPT_NOBODY, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_USERAGENT, eZStaticCache::USER_AGENT);
         $headers = curl_exec($ch);
         $parsedHeaders = array_map(function ($x) {
